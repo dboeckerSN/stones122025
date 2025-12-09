098030909b4d4ed817458f3b2cb0e044b63e6e93
Dominik B <dominik.boecker@sn-invent.de>
2025-12-09 09:23:24 +0100

    Aufgabe 3

--- src/app/product/product-display/product-display.ts
+++ src/app/product/product-display/product-display.ts
@@ -10,9 +10,23 @@ import { Product } from '../product';
 export class ProductDisplay {
   product = input.required<Product>();
   priceChange = output<number>();
+  showPrice = true;
+
+  styleConfig = {
+    borderStyle: 'dashed',
+  };
 
   increasePrice(): void {
     this.product().price += 5;
+    this.priceChange.emit(this.product().price);
+  }
+
+  togglePrice() {
+    this.showPrice = !this.showPrice;
+  }
+
+  changePrice(price: number): void {
+    this.product().price = price;
     this.priceChange.emit(this.product().price);
   }
 }
