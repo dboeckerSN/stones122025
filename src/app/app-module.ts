cf97f9868eece89472b7b4e064f1cf21885e52a5
Dominik B <dominik.boecker@sn-invent.de>
2025-12-09 13:14:02 +0100

    Aufgabe 5

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -4,19 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { ProductModule } from './product/product-module';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
-  declarations: [
-    App
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ProductModule
-  ],
-  providers: [
-    provideBrowserGlobalErrorListeners()
-  ],
-  bootstrap: [App]
+  declarations: [App],
+  imports: [BrowserModule, AppRoutingModule, ProductModule, MatToolbarModule, MatCardModule],
+  providers: [provideBrowserGlobalErrorListeners()],
+  bootstrap: [App],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/product/product-module.ts
+++ src/app/product/product-module.ts
@@ -4,10 +4,20 @@ import { CommonModule } from '@angular/common';
 import { ProductRoutingModule } from './product-routing-module';
 import { ProductDisplay } from './product-display/product-display';
 import { UtilsModule } from '../utils/utils-module';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [ProductDisplay],
-  imports: [CommonModule, ProductRoutingModule, UtilsModule],
+  imports: [
+    CommonModule,
+    ProductRoutingModule,
+    UtilsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+  ],
   exports: [ProductDisplay],
 })
 export class ProductModule {}
