0bb39f13969717e771a25f98995a464df782508e
Dominik B <dominik.boecker@sn-invent.de>
2025-12-11 10:16:44 +0100

    interceptor

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { App } from './app/app';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { appRoutes } from './app/app-routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { loggingInterceptor } from './app/utils/interceptors/logging-interceptor';
 
 bootstrapApplication(App, {
-  providers: [provideRouter(appRoutes, withComponentInputBinding())],
+  providers: [
+    provideRouter(appRoutes, withComponentInputBinding()),
+    provideHttpClient(withInterceptors([loggingInterceptor])),
+  ],
 });
